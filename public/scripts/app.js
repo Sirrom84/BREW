117a5508b01e47e9c55cf2e1bae346e8508bcf35
Natalie Davidovic <natalija.david@hotmail.com>
2021-02-08 08:26:32 +0000

    Create cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
-=======
 $(() => {
 
-$(function () {
-  $.ajax({
-    method: "GET",
-    url: "/books"
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  $(function () {
+    $.ajax({
+      method: "GET",
+      url: "/books"
+    }).done((users) => {
+      for (user of users) {
+        $("<div>").text(user.name).appendTo($("body"));
+      }
+    });;
 
-})
+  });
 
   // 1. function to create items
   // 2. function to render the items
@@ -30,7 +18,7 @@ $(function () {
 
 
 
-//STOPPING HERE FOR NOW THINGS ARE LOOKING GOOD!
+  //STOPPING HERE FOR NOW THINGS ARE LOOKING GOOD!
   $('.reading-button').click(function (event) {
     if ($('.all-items').is(":visible")) {
       $('.all-items').slideUp();
@@ -44,35 +32,24 @@ $(function () {
   const obj = {
     "title": "sherk",
     "date_added": "2019-02-03"
-  }
+  };
 
-  const generateNewElement = function(obj) {
+  const generateNewElement = function (obj) {
     const title = obj.title;
     const dateAdded = obj.date_added;
 
     const $markup = `
     <article class="item">
-      <header>
-        <h1>${title}</h1>
-      </header>
-
-      <footer>
-        <h6>${dateAdded}</h6>
-      </footer>
-  </article>
+      <h3>${title}</h1>
+      <small>${dateAdded}</small>
+    </article>
     `;
 
     const $item = $('.all-items').prepend($markup);
-    console.log("NEW ITEM ADDED:", $item)
+    console.log("NEW ITEM ADDED:", $item);
     return $item;
   };
 
-generateNewElement(obj);
-
+  generateNewElement(obj);
 
 });
-
-
-
-
->>>>>>> bd2a88bbf97d93a4ebaaedbe3c4447c6e1fe01f8
