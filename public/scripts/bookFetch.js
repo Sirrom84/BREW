7aca7d1237fc838fd2b7ba82ece97af5e095a1a1
Rob <morrisrjc@gmail.com>
2021-02-10 22:35:05 +0000

    Working book fetcher

--- public/scripts/bookFetch.js
+++ public/scripts/bookFetch.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
     let title = "";
     let author = "";
 
-$.get("https://www.googleapis.com/books/v1/volumes?q=" + search + "+intitle",function(response){
+$.get(`https://www.googleapis.com/books/v1/volumes?q=${search}`,function(response){
     console.log('HERES MY LOG FOR THE RESPONSE:', response.items)
       for(let i = 0; i < response.items.length; i++) {
 
       title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');
       author=$('<h5 class="center-align white-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
-      img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">get info</button></a>');
+      img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>');
       url= response.items[i].volumeInfo.imageLinks.thumbnail;
       img.attr('src', url);
       title.appendTo('#result');
@@ -27,7 +27,7 @@ $.get("https://www.googleapis.com/books/v1/volumes?q=" + search + "+intitle",fun
       });
 
      }
-     return false;
+     return false; // need this to prevent the form from auto submitting
   });
 
 });
