a1cf360256f87366b62c2d0c1875d8d31496aed1
Rob <morrisrjc@gmail.com>
2021-02-11 09:19:33 +0000

    did more testing

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const axios = require("axios");
 const { response } = require('express');
-let API_KEY = "C25033A4B2C84329A544B107DB021CAB"
+let API_KEY = "C25033A4B2C84329A544B107DB021CAB";
 
 
 //REST
-const userSearch = "xbox"
+const userSearch = "xbox";
 // set up the request parameters
 const params = {
   api_key: API_KEY,
@@ -16,7 +16,7 @@ const params = {
   sort_by: "average_review",
 
 
-}
+};
 
 module.exports = (db) => {
 
@@ -25,24 +25,24 @@ module.exports = (db) => {
     // make the http GET request to Rainforest API
     axios.get('https://api.rainforestapi.com/request', {params})
 
-    .then(response => {
+      .then(response => {
 
 
 
-      res.send(JSON.stringify(response.data.search_results, 0, 2));
-      const search = response.data.search_results;
-      //Just want to display 5 or 10 items to the user
-      search.slice(0,5).forEach(element => {
-        console.log("NAME: " + element["title"] + " IMG: " + element["image"]);
+        res.send(JSON.stringify(response.data.search_results, 0, 2));
+        const search = response.data.search_results;
+        //Just want to display 5 or 10 items to the user
+        search.slice(0,10).forEach(element => {
+          console.log("NAME: " + element["title"] + " IMG: " + element["image"]);
 
-      });
+        });
 
 
       }).catch(error => {
         console.log(error);
-      })
+      });
 
-})
+  });
 
 
 
